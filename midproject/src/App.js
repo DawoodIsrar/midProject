ea582d59bd1880925aaebb420e0e1a41979390e0
Abrar Asghar <asghaabr@cs.net>
2023-05-02 15:14:41 +0500

    git connection

--- midproject/src/App.js
+++ midproject/src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <>
     <h1>Helo</h1>
+    <h1>Git push Test</h1>
     </>
   );
 }
--- midproject/src/index.js
+++ midproject/src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
   <h1>Hello world</h1>
-    {/* <App /> */}
+    {<App /> }
   </>
 );
 

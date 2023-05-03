190032262162243ae1ffe477ce2c23717156f430
Ali Jan <alibangashjan@gmail.com>
2023-05-03 19:36:20 +0500

    styling history page with css

--- midproject/src/components/History.js
+++ midproject/src/components/History.js
@@ -1,22 +1,32 @@
 import React from "react";
 
+
 export default function History() {
   return (
-    <table className="tab">
+   <>
+   <h1 id="historyh1">  History </h1>
+    <table id="table" className="tab">
         <thead>
           <tr> <th>URL</th>
         <th>shortened URL</th>
         <th>Expiry Date</th>
         </tr>
         </thead>
+        <tbody>
        <tr>
         <td>heyyy</td>
         <td>urllllllllllll</td>
         <td>asdfghjk</td>
        </tr>
+       <tr>
+        <td>heyyy</td>
+        <td>urllllllllllll</td>
+        <td>asdfghjk</td>
+       </tr>
+       </tbody>
        
      
     </table>
-    
+    </>
   );
 }

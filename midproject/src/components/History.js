123c349456216c417ef32e57f86c772025300fe4
Dawood <engrdawoodisrar@gmail.com>
2023-05-06 15:29:43 +0500

    functionality for local storage in main and history page is done

--- midproject/src/components/History.js
+++ midproject/src/components/History.js
@@ -1,32 +1,36 @@
 import React from "react";
 
-
 export default function History() {
+  const key = "s-url";
+  var array = localStorage.getItem(key);
+  var data = array ? [JSON.parse(array)] : []; // check if array is null
+  var newdata = data[0]
   return (
-   <>
-   <h1 id="historyh1">  History </h1>
-    <table id="table" className="tab">
-        <thead>
-          <tr> <th>URL</th>
-        <th>shortened URL</th>
-        <th>Expiry Date</th>
-        </tr>
-        </thead>
-        <tbody>
-       <tr>
-        <td>heyyy</td>
-        <td>urllllllllllll</td>
-        <td>asdfghjk</td>
-       </tr>
-       <tr>
-        <td>heyyy</td>
-        <td>urllllllllllll</td>
-        <td>asdfghjk</td>
-       </tr>
-       </tbody>
+    <>
+      <h1 id="historyh1">History</h1>
+      <table id="table" className="tab">
+      <thead>
+           <tr>
+             <th>URL</th>
+             <th>shortened URL</th>
+             <th>Expiry Date</th>
+           </tr>
+         </thead>
+      {newdata.map((history) => (
+         
+         <tbody>
+            <tr  key={history}>
+              <td>{history.url}</td>
+              <td>{history.date}</td> {/* Use correct key name */}
+              {/* <td>{history.expiry}</td> */}
+            </tr>
+          </tbody>
+         
+      
        
-     
-    </table>
+      ))}
+
+</table>
     </>
   );
 }
--- midproject/src/components/Homepage.js
+++ midproject/src/components/Homepage.js
@@ -1,23 +1,52 @@
-import React,{ useState } from "react";
+import React, { useState } from "react";
 import History from "./History";
 import Navbar from "./Navbar";
-
-
 export default function Homepage() {
+  const btn = (e) => {
+    e.preventDefault();
+    var arr = [];
+    const key = "s-url";
+    const existingData = JSON.parse(localStorage.getItem(key)) || [];
+    var data = {
+      url: name,
+      date: date,
+    };
 
-  
+    arr.push(data);
+    const updatedData = [...existingData, data];
+    localStorage.setItem(key, JSON.stringify(updatedData));
+    console.log(localStorage.getItem(key));
+    setDate('')
+    setName('')
+  };
+  const [name, setName] = useState("");
+  const [date, setDate] = useState("");
+
   return (
     <div className="home">
       <h1 class="heading">URL Shortern</h1>
-        <form class="form">
-         <input class="input1" type="Url" id="txt" placeholder="Enter URL" ></input><br></br><br></br>
-         <input class="dte" type="date"></input>
-         <button class="btn" >Get Short URL </button>
-         </form>
-      
-      
+      <form class="form">
+        <input
+          class="input1"
+          type="Url"
+          id="txt"
+          placeholder="Enter URL"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        ></input>
+        <br></br>
+        <br></br>
+        <input
+          class="dte"
+          type="date"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+        ></input>
+       
+        <button type="submit" class="btn" onClick={btn}>
+          Get Short URL{" "}
+        </button>
+      </form>
     </div>
   );
-
-  }
-
+}
